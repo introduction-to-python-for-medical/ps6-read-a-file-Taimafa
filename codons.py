3a0674f64032392cbbcacc56dbf62c9b789a20e5
Taimafa <taima.fakheraldeen@live.biu.ac.il>
2024-12-08 12:36:44 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    path="data/codons.txt"
+    file = open(path)
+    rows = file.readlines()
+    file.close()
+    mapping={}
+    for row in rows:
+      cells=row.strip().split('\t')
+      codon=cells[0]
+      amino_acid=cells[2]
+      mapping[codon]=amino_acid
+    return mapping
 
 
